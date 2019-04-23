9b440a11fa9f501f80fbfc214a2e33ff241f41f9
雁北去 <478760274@qq.com>
2019-04-23 11:17:10 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,17 @@ import Scroll from './scroll/index'
 const scroll = new Scroll({
     target: document.getElementById('gundong'),
     rollH: 500, // 滚动区域高度
+    callBackTop: () => {
+        console.log('重新加载完成')
+        setTimeout(() => {
+            scroll.LoadComplete()
+        }, 1000);
+
+    },
+    callBackDown: () => {
+        console.log('没有更多了')
+        setTimeout(() => {
+            scroll.LoadComplete(true)
+        }, 1000);
+    }
 })
-console.log(scroll)
--- src/scroll/index.ts
+++ src/scroll/index.ts
@@ -34,10 +34,16 @@ class Scroll {
     private reverse: string;  // 滚动反向 top down
     private points: Array<any>;
     private HasTop: boolean; // 是否有下拉刷新
-    private HasDown: boolean; //
+    private HasDown: boolean; // 是否有更多
     private topText: Array<string>; // 顶部文字 有三种状态
-    // private downText: Array<string>; // 顶部文字 有三种状态
-    // private scrollTop: number;// 距离顶部的距离
+    private downText: Array<string>; // 底部部文字 有三种状态
+    private callBackDown: Function;
+    private callBackTop: Function;
+    private topTextObj: HTMLElement;
+    private downTextObj: HTMLElement;
+    private HasMore: boolean;
+    private topBoundary: number;
+    private downBoundary: number
     constructor(options: ScrollOptions) {
         this.ref = options.target;
         this.startY = 0;
@@ -47,14 +53,33 @@ class Scroll {
         this.rollH = this.rollH || 500;
         this.boundary = [50, 0];
         this.points = [];
-        this.topText = ['下拉可以刷新', '释放立即刷新', '数据刷新中'];
-        const downText: Array<string> = ['loading', '没有更多了'];
-        //   this.scrollTop = 0
+        this.topText = options.topText || ['下拉可以刷新', '释放立即刷新', '数据刷新中'];
+        this.downText = options.downText || ['上拉加载更多', '没有更多了'];
+        this.callBackDown = options.callBackDown;
+        this.callBackTop = options.callBackTop;
+        this.HasMore = true;
+        this.HasTop = true;
+        this.HasDown = true;
+        this.topTextObj = null;
+        this.downTextObj = null;
+        this.topBoundary = options.topBoundary || 50
+        this.downBoundary = options.downBoundary || 50
         this.init();
-        //   return this;
     }
     private init(): void {
         //this.scrollTop = this.ref.getBoundingClientRect().top
+        this.HasTop && ((this.topTextObj = document.getElementById('toptext')))
+        this.HasDown && ((this.topTextObj = document.getElementById('downtext')).style.display = 'black')
+        if (this.HasTop) {
+            this.topTextObj = document.getElementById('toptext')
+            this.topTextObj.style.display = 'block'
+            this.updateText('toptext', 0)
+        }
+        if (this.HasDown) {
+            this.downTextObj = document.getElementById('downtext')
+            this.downTextObj.style.display = 'block'
+            this.updateText('downtext', 0)
+        }
         this.calculationBoundary()
         this._handleTouchStart = this._handleTouchStart.bind(this)
         this._handleTouchMove = this._handleTouchMove.bind(this)
@@ -62,6 +87,19 @@ class Scroll {
         this.ref.addEventListener('touchstart', this._handleTouchStart, passive)
         this.ref.addEventListener('touchmove', this._handleTouchMove, passive)
         this.ref.addEventListener('touchend', this._handleTouchEnd, passive)
+    }
+    private updateText(type: string, index: number): void {
+        let list: Array<string> = null
+        let obj: HTMLElement = null
+        if (type === 'toptext') {
+            list = this.topText
+            obj = this.topTextObj
+        } else {
+            list = this.downText
+            obj = this.downTextObj
+        }
+        // const text: string = list[index]
+        obj.innerText !== list[index] && (obj.innerText = list[index])
     }
     private _handleTouchStart(event: TouchEvent): void {
         this.startY = event.targetTouches[0].pageY;
@@ -72,12 +110,35 @@ class Scroll {
      */
     private calculationBoundary() {
         const obj = this.ref.getBoundingClientRect()
-        const contentH = obj.height - 30; //减去头部高度 实际内容高度
+        const contentH = obj.height - this.topTextObj.clientHeight - this.downTextObj.clientHeight; //减去头部高度 实际内容高度
         this.boundary[1] = this.rollH - contentH; // 可以向下滚动的距离  如果是正数 那么不允许向下滚动 就是向下的边界
-        console.log(this.boundary[1])
+        if (this.boundary[1] > 0) {
+            this.HasMore = false
+            this.HasDown = false
+            this.downTextObj.style.display = 'none'
+        }
+    }
+    /**
+     * 上拉下拉 回调完成 滚动区域调整  回调 必需 type 是否还有更多  默认false 有
+     */
+    LoadComplete(type = false) {
+        this.HasMore = !type
+        console.log(type)
+        if (type) {
+
+            this.updateText('downtext', 1) // 没有更多
+        } else {
+            this.updateText('downtext', 0)
+            this.updateText('toptext', 0)
+        }
+
+        // setTimeout(() => {
+        setTransform(this.ref.style, this.translate);
+        this.calculationBoundary()
+        // }, 1000)
     }
     private _handleTouchMove(event: TouchEvent): boolean {
-        const scrollTop = this.ref.getBoundingClientRect().top
+        // const scrollTop = this.ref.getBoundingClientRect().top
         this.end = event.changedTouches[0].pageY
         const diff = this.end - this.startY;
         if (diff < 0) { // 
@@ -90,17 +151,20 @@ class Scroll {
             event.preventDefault();
             this.reverse = 'down'
         }
-        if (this.translate + diff > 100) {
-            setTransform(this.ref.style, 100);
+        if (this.translate + diff > this.topBoundary * 2) {
+            setTransform(this.ref.style, this.topBoundary * 2);
             this.startTime = +new Date(); // 
             return false
+        }
+        if (this.translate + diff > this.topBoundary) { // 可以优化  避免多次重复渲染html 如果在react中 可以选择shouldComponentUpdate 优化...
+            this.updateText('toptext', 1) // 修改文字
         }
         if (this.translate + diff < (this.boundary[1] - this.boundary[0])) {
             setTransform(this.ref.style, this.boundary[1] - this.boundary[0]);
             this.startTime = +new Date(); // 
             return false
         }
-        // 计算滚动值
+        // 跟新滚动值
         setTransform(this.ref.style, this.translate + diff);
         this.startTime = +new Date(); // 
         this.points.push({ time: this.startTime, y: this.end });
@@ -151,28 +215,44 @@ class Scroll {
         // 判断边界
         this.translate += diff;
         // this.translate = this.translate < this.boundary[1] ? this.boundary[1] : this.translate;
-        if (this.translate < this.boundary[1]) { // 触发 down 边界
+        if (this.translate + this.downBoundary < this.boundary[1]) { // 触发 down 边界
             this.translate = this.boundary[1]
-            console.log('我要加载')
+            if (this.HasDown && this.HasMore) {
+                console.log('我要加载')
+                this.callBackDown && this.callBackDown()
+            } else {
+                setTransform(this.ref.style, this.translate)
+            }
+            return false
         }
         //console.log(this.translate)
         if (this.translate > this.boundary[0]) { // 触发 top 边界
-            console.log('我要刷新')
+            this.updateText('toptext', 2)
             this.translate = 0
+            setTransform(this.ref.style, this.topBoundary);
+            if (this.HasTop) {
+                console.log('我要刷新')
+                this.callBackTop && this.callBackTop()
+            }
+            return false
         }
         if (this.translate < this.boundary[0] && this.translate > 0) {
             this.translate = 0
         }
         setTransform(this.ref.style, this.translate);
     }
-    private to() {
-
-    }
 }
 export default Scroll
 
 interface ScrollOptions {
     target: HTMLElement, //运动的对象
-    rollH: number; // 滚动区域的高度 必需 
-    maxH?: number; // 最大高度 不传 会默认获取 target的高度
+    rollH?: number; // 滚动区域高度 默认 500px
+    topBoundary?: number; // 上拉边界值 默认 50px 拉动超过50才算刷新
+    downBoundary?: number;
+    topText?: Array<string>; // ['下拉可以刷新', '释放立即刷新', '数据刷新中']
+    downText?: Array<string>; // ['上拉加载更多', '没有更多了'];
+    HasTop?: boolean; // 默认 true
+    HasDown?: boolean; // 默认 true
+    callBackTop?: Function; // 上拉回调
+    callBackDown?: Function; // 下拉回调
 }
